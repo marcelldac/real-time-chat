947f2cd53bcbf38415d866a8a4eb80449fb6d041
marcelldac <dacmarcell@gmail.com>
2023-10-15 23:49:00 -0300

    feat: add chatengine api

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const PORT = 3001;
 
@@ -9,9 +10,20 @@ app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
+
+  try{
+    const response = await axios.put(
+      'https://api.chatengine.io/users/',
+      {username: username, secret: username, first_name: username},
+      {headers: {"private-key": "e046e74f-9b57-467f-b55c-8160c96d4f61"}}
+    )
+    return res.status(response.status).json(response.data);
+  }catch(error){
+    return res.status(error.response.status).json(error.response.data);
+  }
   return res.json({ username: username, secret: "sha256..." });
 });
 
 app.listen(PORT, () => {
-    console.info(`Listenning on port ${PORT}`);
+    console.info(`Listening on port ${PORT}`);
 });
